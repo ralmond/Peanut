b40e22b89d59592d304be344be77a6e8a1b52374
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2016-05-02 14:22:26 +0000

    QOmega

---
+++ Peanut/trunk/R/QOmegaMat.R
@@ -0,0 +1,138 @@
+
+### List of rules which should be considered offset functions
+OffsetRules <- c("OffsetConjunctive","OffsetDisjunctive")
+getOffsetRules() { OffsetRules}
+setOffsetRules(newval) {
+  assignInNamesspace("OffsetRules",newval,"Peanut")
+}
+
+
+## This is clipboard inheritance from RNetica, but as Pnets is not
+## supposed to depend on RNetica, this seems the easiest way to do this.
+dputToString <- function (obj) {
+  con <- textConnection(NULL,open="w")
+  tryCatch({dput(obj,con);
+           textConnectionValue(con)},
+           finally=close(con))
+}
+
+dgetFromString <- function (str) {
+  con <- textConnection(str,open="r")
+  tryCatch(dget(con), finally=close(con))
+}
+
+
+
+
+### Function to build a blank Q-matrix from a Bayes net.
+Pnet2Qmat <- function (pnet,obs,prof) {
+  statecounts <- sapply(obs,NodeNumStates)
+  obsnames <- sapply(obs,NodeName)
+  profnames <- sapply(prof,NodeName)
+
+  ## Lay out node name row with proper repetition structure
+  rowcounts <- statecounts-1
+  Node <- rep(names(statescounts),each=rowcounts)
+  nrow <- length(nodenames)
+
+  ## Now lay out blank structure of rest of Qmat
+  NStates <- integer(nrow)
+  States <- character(nrow)
+  Link <- character(nrow)
+  LinkScale <- numeric(nrow)
+  Q <- matrix(NA,nrow,length(profnames))
+  colnames(Q) <- profnames
+  Rules <- character(nrow)
+  A <- matrix(NA,nrow,length(profnames))
+  colnames(A) <- profnames
+  B <- numeric(nrow)
+  PriorWeight <- numeric(nrow)
+
+  ## Now loop over vars, processing each one.
+  irow <- 1
+  for (nd in obs) {
+    ## first row
+    NStates[irow] <- nstate <- NodeNumStates(nd)
+    States[irow:(irow+nstate-2)] <- NodeStates(nd)[1:(nstate-1)]
+    Link[irow] <- as.character(PnodeLink(nd))
+    LinkScale[irow] <- as.character(PnodeLinkScale(nd))
+    ## Q -- if not supplied, fill in first row according to parents.
+    ## If supplied, reproduce matrix.
+    if (is.null(PnodeQ(nd))) {
+      Q[irow,] <- profname %in% NodeParentNames(nd)
+    } else {
+      Q[irow:(irow+nstate-2),] <- PnodeQ(nd)
+    }
+    if (length(PnodeRules(nd)) == 1 || class(PnodeRules(nd))==function) {
+      Rules[irow] <- dputToString(PnodeRules(nd))
+    } else {
+      Rules[irow:(irow+nstate-2)] <- sapply(PnodeRules(nd),dputToString)
+    }
+    ### HERE
+    ## A  -- LnAlpha or Beta if Rule is OffsetXXX
+    ## B -- Beta or LnAlpha if Rule is OffsetXXX
+    if (length(PnodeRules(nd)) ==1 || class(PnodeRules(nd))) {
+      if (PnodeRules(nd) %in% getOffsetRules()) {
+        a <- PnodeLnAlphas(nd)
+        b <- PnodeBetas(nd)
+      } else {
+        a <- PnodeLnAlphas(nd)
+        b <- PnodeBetas(nd)
+      }
+    } else {
+      ## Different rule for each row of table, may need different A's
+      ## and B's
+      alpha <- PnodeLnAlphas(nd)
+      beta <- PnodeBetas(nd)
+      if (!is.list(alpha)) {
+        alpha <- list(alpha)
+        if (nstate>2) {
+          for (i in 2:(nstate-1)) {
+            alpha[[i]] <- alpha[[1]]
+          }
+        }
+      }
+      if (!is.list(beta)) {
+        beta <- list(beta)
+        if (nstate>2) {
+          for (i in 2:(nstate-1)) {
+            beta[[i]] <- beta[[1]]
+          }
+        }
+      }
+      a <- alpha; b <- beta
+      i <- 1
+      for (rule in PnodeRules(nd)) {
+        if (rule %in% OffsetRules) {
+          a[[i]] <- beta[[i]]
+          b[[i]] <- alpha[[i]]
+        }
+        i <- i+1
+      }
+    }
+    ## Now write out a
+    ## Easier to just handle list case
+    if (!is.list(a)) a <- list(a)
+    for (i in 1:length(a)) {
+      aa <- a[i]
+      if (is.null(names(aa))) {
+        names(aa) <- ParentNames(nd)
+      }
+      A[irow+i-1,names(aa)] <- aa
+    }
+    ## For b, easier to coerce to vector.
+    b <- as.numeric(b)
+    if (length(b) > nstate-1) {
+      stop("Too much b:", b)
+    }
+    B[irow:(irow+length(b)-1)] <- b
+
+    ## Weights
+
+    ## Next node
+    irow <- irow + nstate-1
+  }
+  ## Finally, put this togehter into a data frame
+  result <- data.frame(Node,States,Link,LinkScale,Q,Rules,A,B,PriorWeight)
+  class(result) <- c("Qmat",class(result))
+}
