e6ad090364e762157d26dccdae5eabf6d0e851e4
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2016-11-07 22:47:04 +0000

    SeyfullahsTest

--- R/EM.R
+++ R/EM.R
@@ -1,25 +1,28 @@
 
 GEMfit <- function (net, cases, tol=sqrt(.Machine$double.eps), maxit=100,
-                   Estepit=1, Mstepit=3) {
+                   Estepit=1, Mstepit=3,trace=FALSE,debugNo=maxit+1) {
 
   ## Base case
   converged <- FALSE
   llike <- rep(NA,maxit+1)
   iter <- 1
-  BuildAllTables(net)
+  BuildAllTables(net,debug=iter>=debugNo)
   llike[iter] <- calcPnetLLike(net,cases)
+  if (trace) cat("Iteration ",iter,"; Log likelihood",llike[iter],"\n")
 
   while(!converged && iter <= maxit) {
     ## E-step
     calcExpTables(net, cases, Estepit=Estepit, tol=tol)
 
     ## M-step
-    maxAllTableParams(net, Mstepit=Mstepit, tol=tol)
+    maxAllTableParams(net, Mstepit=Mstepit, tol=tol,
+                      debug=iter>=debugNo)
 
     ## Update parameters & convergence test
     iter <- iter + 1
-    BuildAllTables(net)
+    BuildAllTables(net,debug=iter>=debugNo)
     llike[iter] <- calcPnetLLike(net,cases)
+    if (trace) cat("Iteration ",iter,"; Log likelihood",llike[iter],"\n")
     converged <- (abs(llike[iter]-llike[iter-1]) < tol)
   }
 
@@ -49,14 +52,19 @@ calcExpTables <- function (net, cases, Estepit=1, tol=sqrt(.Machine$double.eps))
   UseMethod("calcExpTables")
 }
 
-maxAllTableParams <- function (net, Mstepit=5, tol=sqrt(.Machine$double.eps)) {
+maxAllTableParams <- function (net, Mstepit=5, tol=sqrt(.Machine$double.eps),
+                               debug=FALSE) {
   UseMethod("maxAllTableParams")
 }
 
 maxAllTableParams.default <- function (net, Mstepit=5,
-                                       tol=sqrt(.Machine$double.eps)) {
+                                       tol=sqrt(.Machine$double.eps),
+                                       debug=FALSE) {
   lapply(PnetPnodes(net),
-         function (nd) {maxCPTParam(nd,Mstepit,tol)})
+         function (nd) {
+           if (debug) cat("Updating params for",nd,"\n")
+           maxCPTParam(nd,Mstepit,tol)
+         })
   invisible(net)
 }
 
